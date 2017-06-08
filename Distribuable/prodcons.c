e9d8dd807c45b37ec35292d81b0e6d2e6580f6fe
Louis Freret <louis.freret@etu.utc.fr>
2017-06-08 17:01:05 +0200

    prodcons with fifo

--- Distribuable/prodcons.c
+++ Distribuable/prodcons.c
@@ -5,22 +5,13 @@
 
 #include "serialio.h"
 #include "noyau.h"
+#include "fifo.h"
 
 /*
  ** Test du noyau preemptif. Lier noyautes.c avec noyau.c et noyaufil.c
  */
 
 /* Création de la file du producteur/consommateur */
-
-#define MAX_FILE  6
-
-typedef struct FIFO
-{
-  int file[MAX_FILE];
-  int head;
-  int tail;
-  int size;
-} FIFO;
 FIFO fifo;
 
 /* Création des tâches */
@@ -33,9 +24,8 @@ uint16_t Cons;
 
 TACHE launch(void) {
   puts("------> EXEC tache launch");
-  fifo.head = 0;
-  fifo.tail = 0;
-  fifo.size = 0;
+  init_fifo(&fifo);
+
   Prod = cree(Producteur);
   Cons = cree(Consommateur);
   active(Prod);
@@ -48,13 +38,10 @@ TACHE Producteur(void) {
   long j;
   puts("------> DEBUT tache Producteur");
   while(1) {
-    fifo.file[fifo.tail] = 1;
-    fifo.tail = (fifo.tail + 1) % MAX_FILE;
-    fifo.size = fifo.size + 1;
-
-    if (fifo.size == MAX_FILE)
+    push_fifo(&fifo, 1);
+    if (size_fifo(&fifo) == MAX_FILE)
       dort();
-    if (fifo.size == 1)
+    if (size_fifo(&fifo) == 1)
       reveille(Cons);
   }
   fin_tache();
@@ -65,13 +52,10 @@ TACHE Consommateur(void) {
   long j;
   puts("------> DEBUT tache Consommateur");
   while(1) {
-    fifo.file[fifo.head] = 0;
-    fifo.head = (fifo.head + 1) % MAX_FILE;
-    fifo.size = fifo.size - 1;
-
-    if(fifo.size == 0)
+    pop_fifo(&fifo);
+    if(size_fifo(&fifo) == 0)
       dort();
-    if(fifo.size == MAX_FILE - 1)
+    if(size_fifo(&fifo) == MAX_FILE - 1)
       reveille(Prod);
   }
   fin_tache();
